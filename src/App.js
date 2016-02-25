e1b958e34a9c507f8213c888c78ae76998ad7b59
richardnd5 <nathan.d.richard@gmail.com>
2016-02-24 20:16:22 -0700

    createdCommentApp
    Wow

--- src/App.js
+++ src/App.js
@@ -1,39 +1,122 @@
-import React, { Component } from 'react';
-import { NICE, SUPER_NICE } from './colors';
+var React = require('react');
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
+var App = React.createClass({
 
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    });
-  }
+  getInitialState: function() {
+    return{
+      comments: [
+        {
+          name: 'Koala',
+          message: 'Pleasant'
+        },
+        {
+          name: 'Yarrow',
+          message: 'What have you?'
+        }
+      ]
+    }
+  },
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
+handleFormSubmit: function(comment) {
+  var comments = this.state.comments;
+  var newComments = comments.concat([comment])
 
-  render() {
-    return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
-    );
-  }
+  this.setState({
+    comments: newComments
+
+  });
+},
+
+render: function() {
+  return (
+
+    <div>
+      <CommentForm formSubmit={this.handleFormSubmit} />
+    <CommentList comments={this.state.comments}/>
+    </div>
+  )
 }
+});
 
-export class App extends Component {
-  render() {
+var CommentForm = React.createClass({
+
+  getInitialState: function() {
+    return {
+      name: '',
+      message: ''
+    }
+  },
+
+  handleChange: function(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    this.props.formSubmit(this.state);
+
+    this.setState({
+      name: '',
+      message: ''
+    })
+  },
+
+
+  render: function() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            placeholder="Your Name"
+          />
+          <input
+            type="text"
+            name='message'
+            value={this.state.message}
+            onChange={this.handleChange}
+            placeholder='Speak it.'
+
+          />
+          <input
+            type="submit"
+          />
+
+        </form>
+    )
+  }
+});
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+
+      <div>
+        {this.props.comments.map(function(comment, i) {
+              return (
+              <Comment name={comment.name} message= {comment.message} key={i} />
+              )
+            })}
+      </div>
+    )
+  }
+});
+
+var Comment = React.createClass({
+
+  render: function() {
     return (
       <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
+        <h3>{this.props.name}</h3>
+        <p>{this.props.message}</p>
       </div>
-    );
+    )
   }
-}
+});
+
+module.exports = App;
